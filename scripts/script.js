0cc34c67027a44c0171144978bd659627b6ec7ac
Henry Kanemoto <kanemotohenry@gmail.com>
2026-06-26 16:00:43 -0300

    Adicion Mensagem de boas vindas

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,7 @@ function mostrarMensagem() {
     document.getElementById("mensagem").innerHTML = 
         "Parabéns! Seu projeto está funcionando corretamente.";
 }
+
+window.onload = () => {
+    alert("Bem vindo ao Site Full Stack tarde!");
+}
